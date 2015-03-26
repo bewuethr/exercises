9bfcf804575a288fb992f3d601e70b9ecadb541e
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-03-26 18:04:43 -0400

    Add Chapter 2, exercise 1

---
+++ chapter02/chapter02_ex01.pl
@@ -0,0 +1,6 @@
+#!C:\strawberry\perl\bin\perl.exe
+
+# Compute circumference of a circle with radius of 12.5
+
+$c = 2 * 3.14159 * 12.5;
+print "The circumference of a circle with radius 12.5 is $c.\n";
