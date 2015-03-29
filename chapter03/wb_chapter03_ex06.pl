cfb911996d59be7bb917c178eee425c79b485ea3
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-03-29 01:27:45 -0400

    Add Workbook Chapter 3, exercise 6

---
+++ chapter03/wb_chapter03_ex06.pl
@@ -0,0 +1,8 @@
+#!C:\strawberry\perl\bin\perl.exe
+
+# Use the range operator to create a list of only lowercase, only uppercase and
+# a list of both
+
+print 'a'..'z', "\n";
+print 'A'..'Z', "\n";
+print 'A'..'Z', 'a'..'z', "\n"; # not possible with just one range operator
