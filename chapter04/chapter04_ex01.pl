88cbb32d5d33b8f20b812fb2724b24211c033f4d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-03-29 05:15:09 -0400

    Add Chapter 4, exercise 1

---
+++ chapter04/chapter04_ex01.pl
@@ -0,0 +1,21 @@
+#!C:\strawberry\perl\bin\perl.exe
+
+# Subroutine total that returns total of a list of numbers
+
+use warnings;
+use strict;
+
+sub total {
+    my $sum;
+    foreach (@_) {
+        $sum += $_;
+    }
+    $sum;
+}
+
+my @fred = qw{ 1 3 5 7 9 };
+my $fred_total = total(@fred);
+print "The total of \@fred is $fred_total.\n";
+print "Enter some numbers on separate lines: ";
+my $user_total = total(<STDIN>);
+print "The total of those numbers is $user_total.\n";
