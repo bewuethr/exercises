ee9445aec30fc3b630c44fbe43db9336e8f385dc
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-03-30 15:45:19 -0400

    Add use pragmas to Workbook Chapter 5, exercise 9

--- chapter05/wb_chapter05_ex09.pl
+++ chapter05/wb_chapter05_ex09.pl
@@ -3,6 +3,9 @@
 # Take filename from command line; if file exists, print first line, otherwise
 # die
 
+use 5.020;
+use warnings;
+
 my $file = shift @ARGV;
 open my $fh, '<', $file
     or die "Could not open $file: $!";
