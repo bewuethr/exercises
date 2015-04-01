94bf7b04fc46b95aeead6ce874ae157bf88bbbb6
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-03-31 20:46:21 -0400

    Add Workbook Chapter 7, exercise 3

---
+++ chapter07/wb_chapter07_ex03.pl
@@ -0,0 +1,15 @@
+#!C:\strawberry\perl\bin\perl.exe
+
+# Match line that contains 'a', 'b' and 'c', in that order; at least one
+# character between
+
+print "Enter string: ";
+while (<STDIN>) {
+    chomp;
+    if ( /a.+b.+c/ ) {  # does not match newlines, use /s flag or [\d\D]*
+        print "Match!\n";
+    }
+    else {
+        print "No match\n";
+    }
+}
