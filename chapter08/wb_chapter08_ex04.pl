7bf7d58f59b199b6cda6a70929d68789d2e2261d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-04-01 01:31:22 -0400

    Remove unnecessary comment

--- chapter08/wb_chapter08_ex04.pl
+++ chapter08/wb_chapter08_ex04.pl
@@ -7,7 +7,7 @@ use warnings;
 
 while (<>) {    # take one input line at a time
     chomp;
-    if (/\A(.+).*\g{1}\z/) {    # at end
+    if (/\A(.+).*\g{1}\z/) {
         print "Matched: |$`<$&>$'|\n";    # the special match vars
         print "'$1' at beginning and end\n";
     }
