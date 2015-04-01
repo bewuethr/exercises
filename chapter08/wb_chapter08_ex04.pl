aaae966b0aa8ff92c35ce85545fafc2d428525b2
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-04-01 01:25:54 -0400

    Add Workbook Chapter 8, exercise 4

---
+++ chapter08/wb_chapter08_ex04.pl
@@ -0,0 +1,17 @@
+#!C:\strawberry\perl\bin\perl.exe
+
+# Match string that begins and ends with the same thing (without overlap)
+
+use 5.020;
+use warnings;
+
+while (<>) {    # take one input line at a time
+    chomp;
+    if (/\A(.+).*\g{1}\z/) {    # at end
+        print "Matched: |$`<$&>$'|\n";    # the special match vars
+        print "'$1' at beginning and end\n";
+    }
+    else {
+        print "No match: |$_|\n";
+    }
+}
