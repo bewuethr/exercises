efb70db0c94212aa8fb3ee8b1f76af9c7859a49e
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2015-04-05 18:05:15 -0400

    Add Chapter 12, exercise 1

---
+++ chapter12/chapter12_ex01.pl
@@ -0,0 +1,16 @@
+#!C:\strawberry\perl\bin\perl.exe
+
+# Take list of file names from command line; report for each if it's readable,
+# writable, executable or doesn't exist
+
+use strict;
+use warnings;
+
+foreach my $fname (@ARGV) {
+    printf "%-30s: ", $fname;
+    print "readable" if -r $fname;
+    print " writable" if -w $fname; # or use the special _ filehandle
+    print " executable" if -x $fname;
+    print "inexisting" unless -e $fname;
+    print "\n";
+}
