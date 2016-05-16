4143b5924dcfda55573cb872797019c3d2cb5f22
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2016-05-16 01:54:48 -0400

    Add Chapter 05, exercise 04

---
+++ chapter05/chapter05_ex04.js
@@ -0,0 +1,15 @@
+function every(array, test) {
+    for (var i = 0; i < array.length; ++i) {
+        if (!test(array[i]))
+            return false;
+    }
+    return true;
+}
+
+function some(array, test) {
+    for (var i = 0; i < array.length; ++i) {
+        if (test(array[i]))
+            return true;
+    }
+    return false;
+}
