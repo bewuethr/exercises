1fa54a9190e09b284e5dbfdb05438d905a252ac6
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2016-05-26 01:12:42 -0400

    Add Chapter 07, exercise 02

--- chapter07/chapter07.js
+++ chapter07/chapter07.js
@@ -1,3 +1,4 @@
+// Exercise 1
 function SmartPlantEater() {
     this.energy = 20;
     this.dir = "s";
@@ -6,9 +7,9 @@ SmartPlantEater.prototype.act = function(view) {
     var space = view.find(" ");
     if (this.energy > 60 && space)
         return {type: "reproduce", direction: space};
-    var plants = view.findAll("*");
 
     // Only eat plants if there will be at least one left afterwards
+    var plants = view.findAll("*");
     if (plants.length > 1)
         return {type: "eat", direction: plants[0]};
 
@@ -24,3 +25,34 @@ SmartPlantEater.prototype.act = function(view) {
         return {type: "move", direction: this.dir};
     }
 };
+
+// Exercise 2
+
+function Tiger() {
+    this.energy = 50;
+    this.direction = randomElement(directionNames);
+    this.preyContacts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+Tiger.prototype.act = function(view) {
+    var space = view.find(" ");
+    if (this.energy > 100 && space)
+        return {type: "reproduce", direction: space};
+
+    // Update array of number of prey seen in the last 10 rounds
+    this.preyContacts.shift();
+    this.preyContacts.push(view.findAll("O").length);
+    var preyCount = this.preyContacts.reduce(function(a, b) {
+        return a+b;
+    });
+    var prey = view.find("O");
+
+    // Only eat if energy is low or prey count high
+    if (prey && (this.energy < 40 || preyCount > 0))
+        return {type: "eat", direction: prey};
+
+    if (space) {
+        if (view.look(this.direction) != " ")
+            this.direction = space;
+        return {type: "move", direction: this.direction};
+    };
+}
