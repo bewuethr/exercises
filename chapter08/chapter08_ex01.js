f393858ab8eab2ff4899fd8e8dcda41dbbd43eb8
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2016-05-27 01:16:17 -0400

    Add Chapter 08, exercise 01

---
+++ chapter08/chapter08_ex01.js
@@ -0,0 +1,11 @@
+function reliableMultiply(a, b) {
+    for (;;) {
+        try {
+            return primitiveMultiply(a, b);
+        }
+        catch (e) {
+            if (!(e instanceof MultiplicatorUnitFailure))
+                throw e;
+        }
+    }
+}
