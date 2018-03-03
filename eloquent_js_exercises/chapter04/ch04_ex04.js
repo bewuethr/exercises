2104300499b613b4d6027a3be4fe9a961813ca45
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-03 02:25:59 -0500

    Add Chapter 04, exercise 4

--- eloquent_js_exercises/chapter04/ch04_ex04.js
+++ eloquent_js_exercises/chapter04/ch04_ex04.js
@@ -1,25 +1,15 @@
-// Number of object properties
-function propCount(obj) {
-    var ctr = 0;
-    for (var prop in obj)
-        ++ctr;
-    return ctr;
-}
-
 function deepEqual(obj1, obj2) {
-    if (typeof obj1 == "object" && obj1 != null &&
-        typeof obj2 == "object" && obj2 != null)
+    if (typeof obj1 != "object" || obj1 == null ||
+        typeof obj2 != "object" || obj2 == null)
     {
-        if (propCount(obj1) !== propCount(obj2))
+        return obj1 === obj2;
+    } else {
+        if (Object.keys(obj1).length != Object.keys(obj2).length) {
             return false;
-        for (var prop in obj1) {
-            if (! prop in obj2)
-                return false;
-            if (deepEqual(obj1[prop], obj2[prop]) === false)
-                return false;
+        }
+        for (let key of Object.keys(obj1)) {
+            if (!(obj2[key] && deepEqual(obj1[key], obj2[key]))) return false;
         }
         return true;
     }
-    else
-        return obj1 === obj2;
 }
