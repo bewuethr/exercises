8848ba7bbe73f626aa34b07a279874873b379483
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-03 02:33:44 -0500

    Changes after review

--- eloquent_js_exercises/chapter04/ch04_ex04.js
+++ eloquent_js_exercises/chapter04/ch04_ex04.js
@@ -1,15 +1,14 @@
 function deepEqual(obj1, obj2) {
     if (typeof obj1 != "object" || obj1 == null ||
-        typeof obj2 != "object" || obj2 == null)
-    {
-        return obj1 === obj2;
-    } else {
-        if (Object.keys(obj1).length != Object.keys(obj2).length) {
-            return false;
-        }
-        for (let key of Object.keys(obj1)) {
-            if (!(obj2[key] && deepEqual(obj1[key], obj2[key]))) return false;
-        }
-        return true;
+        typeof obj2 != "object" || obj2 == null) return obj1 === obj2;
+
+    let keys1 = Object.keys(obj1), keys2 = Object.keys(obj2);
+
+    if (keys1.length != keys2.length) return false;
+
+    for (let key of keys1) {
+        if (!(keys2.includes(key) &&
+              deepEqual(obj1[key], obj2[key]))) return false;
     }
+    return true;
 }
