295431b5959bdfbe25d433c565aef9c9b61e4b85
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-05 01:13:09 -0500

    Add empty array as initial value

--- eloquent_js_exercises/chapter05/chapter05_ex01.js
+++ eloquent_js_exercises/chapter05/chapter05_ex01.js
@@ -1,5 +1,5 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
-let array = arrays.reduce((a, b) => a.concat(b));
+let array = arrays.reduce((a, b) => a.concat(b), []);
 console.log(array);
 // → [1, 2, 3, 4, 5, 6]
