b52cc64da7f2ecf037c5b3226a4c6641fe4c4b73
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-07 00:34:02 -0500

    Add Chapter 06, exercise 1

--- eloquent_js_exercises/chapter06/chapter06_ex01.js
+++ eloquent_js_exercises/chapter06/chapter06_ex01.js
@@ -1,18 +1,22 @@
-function Vector(x, y) {
-    this.x = x;
-    this.y = y;
-}
-
-Vector.prototype.plus = function(vec) {
-    return new Vector(this.x + vec.x, this.y + vec.y);
-}
-
-Vector.prototype.minus = function(vec) {
-    return new Vector(this.x - vec.x, this.y - vec.y);
-}
-
-Object.defineProperty(Vector.prototype, "length", {
-    get: function() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
-});
+
+    plus(v) {
+        return new Vec(this.x + v.x, this.y + v.y);
+    }
+
+    minus(v) {
+        return new Vec(this.x - v.x, this.y - v.y);
+    }
+
+    get length() {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
+
+    toString() {
+        return `Vec{x: ${this.x}, y: ${this.y}}`;
+    }
+}
