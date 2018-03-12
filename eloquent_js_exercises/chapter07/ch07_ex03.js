72b18ec7cf61c6f99b0b2e94847b00fb46979e12
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-12 10:41:26 -0400

    Simplify constructor, don't copy if unchanged

--- eloquent_js_exercises/chapter07/ch07_ex03.js
+++ eloquent_js_exercises/chapter07/ch07_ex03.js
@@ -1,34 +1,21 @@
 class PGroup {
-	constructor() {
-		this.members = [];
+	constructor(members) {
+		this.members = members;
 	}
 
 	add(elem) {
-		let newMembers = this.members.slice(0);
-		if (!newMembers.includes.elem) {
-			newMembers.push(elem);
-		}
-		return PGroup.from(newMembers);
+		if (this.has(elem)) return this;
+		return new PGroup(this.members.concat(elem));
 	}
 
 	delete(elem) {
-		let newMembers = this.members.filter(e => e != elem);
-		return PGroup.from(newMembers);
+		if (!this.has(elem)) return this;
+		return new PGroup(this.members.filter(e => e !== elem));
 	}
 
 	has(elem) {
 		return this.members.includes(elem);
 	}
-
-	static from(collection) {
-		let group = new PGroup();
-		for (let elem of collection) {
-			if (!group.members.includes(elem)) {
-				group.members.push(elem);
-			}
-		}
-		return group;
-	}
 }
 
-PGroup.empty = PGroup.from([]);
+PGroup.empty = new PGroup([]);
