a98b3310ffc32c718f7b37d5fd8a8ee9ff9a8a9d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-12 21:04:23 -0400

    Update indentation [#1]

--- eloquent_js_exercises/chapter07/ch07_ex03.js
+++ eloquent_js_exercises/chapter07/ch07_ex03.js
@@ -1,21 +1,21 @@
 class PGroup {
-	constructor(members) {
-		this.members = members;
-	}
+    constructor(members) {
+        this.members = members;
+    }
 
-	add(elem) {
-		if (this.has(elem)) return this;
-		return new PGroup(this.members.concat(elem));
-	}
+    add(elem) {
+        if (this.has(elem)) return this;
+        return new PGroup(this.members.concat(elem));
+    }
 
-	delete(elem) {
-		if (!this.has(elem)) return this;
-		return new PGroup(this.members.filter(e => e !== elem));
-	}
+    delete(elem) {
+        if (!this.has(elem)) return this;
+        return new PGroup(this.members.filter(e => e !== elem));
+    }
 
-	has(elem) {
-		return this.members.includes(elem);
-	}
+    has(elem) {
+        return this.members.includes(elem);
+    }
 }
 
 PGroup.empty = new PGroup([]);
