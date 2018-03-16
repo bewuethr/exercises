6a7dc4f4e3e1e0ac0bd3922f022ce08b12ffd535
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-16 01:30:54 -0400

    Add Chapter 09, exercise 2

--- eloquent_js_exercises/chapter09/chapter09_ex02.js
+++ eloquent_js_exercises/chapter09/chapter09_ex02.js
@@ -1,6 +1,4 @@
-var text = "'I'm the cook,' he said, 'it's my job.'";
-
-console.log(
-        text.replace(/([a-z])'([a-z])/gi, "$1~~$2")
-        .replace(/'/g, "\"")
-        .replace(/~~/g, "'"));
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/(\W|^)'|'(\W|$)/g, '$1"$2'));
+// → "I'm the cook," he said, "it's my job."
