56ad42e60d11d8c8f4e231e02368fb478dfb6c1c
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-28 00:45:36 +0200

    Add Chapter 12, exercise 4

---
+++ eloquent_js_exercises/chapter12/ch12_ex04.js
@@ -0,0 +1,16 @@
+specialForms.set = (args, scope) => {
+    if (args.length != 2 || args[0].type != "word") {
+        throw new SyntaxError("Incorrect use of set");
+    }
+    let curScope = scope;
+    while (!Object.prototype.hasOwnProperty.call(curScope, args[0].name)) {
+        curScope = Object.getPrototypeOf(curScope);
+        if (curScope === null) {
+            throw new ReferenceError("Can't set non-existing binding " +
+                                     `'${args[0].name}'`);
+        }
+    }
+    let value = evaluate(args[1], scope);
+    curScope[args[0].name] = value;
+    return value;
+}
