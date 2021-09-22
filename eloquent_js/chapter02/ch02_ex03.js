d1bf5816567a1ab780817695b6919e0def9c15de
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-09-23 00:20:16 +0200

    Build single string

--- eloquent_js/chapter02/ch02_ex03.js
+++ eloquent_js/chapter02/ch02_ex03.js
@@ -1,9 +1,11 @@
 let size = 8;
+let line = "";
 
 for (let i = 0; i < size; i++) {
-	let line = "";
 	for (let j = 0; j < size; j++) {
 		line += i%2 == j%2 ? " " : "#";
 	}
-	console.log(line);
+	line += "\n";
 }
+
+console.log(line);
