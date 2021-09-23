591001d48e2191e73091df20b13e6bf31d7a2bc0
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-09-24 00:31:27 +0200

    Add chapter 3, exercise 3

--- eloquent_js/chapter03/ch03_ex03.js
+++ eloquent_js/chapter03/ch03_ex03.js
@@ -1,20 +1,12 @@
-function countBs(s) {
-    let count = 0;
-    for (let i = 0; i < s.length; ++i) {
-        if (s[i] == "B") ++count;
-    }
-    return count;
-}
-
 function countChar(s, c) {
-    let count = 0;
-    for (let i = 0; i < s.length; ++i) {
-        if (s[i] == c) ++count;
-    }
-    return count;
+	let count = 0;
+	for (let i = 0; i < s.length; ++i) {
+		if (s[i] == c) ++count;
+	}
+
+	return count;
 }
 
-console.log(countBs("BBC"));
-// → 2
-console.log(countChar("kakkerlak", "k"));
-// → 4
+function countBs(s) {
+	return countChar(s, "B");
+}
