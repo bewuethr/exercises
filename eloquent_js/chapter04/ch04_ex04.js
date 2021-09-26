c5a58c6f3dd202a2a6f645d0d36a41982029dbb2
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-09-27 00:24:27 +0200

    Compare number of keys as well

--- eloquent_js/chapter04/ch04_ex04.js
+++ eloquent_js/chapter04/ch04_ex04.js
@@ -1,6 +1,8 @@
 function deepEqual(a, b) {
 	if (!(isObject(a) && isObject(b))) return a === b;
 
+	if (Object.keys(a).length != Object.keys(b).length) return false;
+
 	for (let key of Object.keys(a)) {
 		if (!(key in b)) return false;
 		if (!deepEqual(a[key], b[key])) return false;
