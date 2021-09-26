501defe76d1cb866d7660c22446226fc9e30f8e9
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-09-26 23:04:20 +0200

    Add chapter 4, exercise 1

--- eloquent_js/chapter04/ch04_ex01.js
+++ eloquent_js/chapter04/ch04_ex01.js
@@ -1,21 +1,17 @@
-function range(start, end, step = start < end ? 1 : -1) {
-    let arr = [];
-    if (step > 0) {
-        for (let n = start; n <= end; n += step) {
-            arr.push(n);
-        }
-    } else if (step < 0) {
-        for (let n = start; n >= end; n += step) {
-            arr.push(n);
-        }
-    }
-    return arr;
+function range(start, end, step = 1) {
+	let arr = [];
+	for (let n = start; Math.abs(n - start) <= Math.abs(end - start); n += step) {
+		arr.push(n);
+	}
+
+	return arr;
 }
 
 function sum(arr) {
-    let s = 0;
-    for (let num of arr) {
-        s += num;
-    }
-    return s;
+	let sum = 0;
+	for (let n of arr) {
+		sum += n;
+	}
+
+	return sum;
 }
