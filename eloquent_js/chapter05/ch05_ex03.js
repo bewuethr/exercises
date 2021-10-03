0887eb5bc55d9dfad17f1ea23a9ff9422a268f69
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-10-03 11:11:44 +0200

    Add chapter 5, exercise 3

--- eloquent_js/chapter05/ch05_ex03.js
+++ eloquent_js/chapter05/ch05_ex03.js
@@ -1,24 +1,13 @@
-function every1(array, test) {
-    for (let elem of array) {
-        if (!test(elem)) return false;
-    }
-    return true;
+function every(array, test) {
+	for (let el of array) {
+		if (!test(el)) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
-function every2(array, test) {
-    return !array.some(elem => !test(elem));
+function everySome(array, test) {
+	return !array.some(n => !test(n));
 }
-
-console.log(every1([1, 3, 5], n => n < 10));
-// → true
-console.log(every1([2, 4, 16], n => n < 10));
-// → false
-console.log(every1([], n => n < 10));
-// → true
-
-console.log(every2([1, 3, 5], n => n < 10));
-// → true
-console.log(every2([2, 4, 16], n => n < 10));
-// → false
-console.log(every2([], n => n < 10));
-// → true
