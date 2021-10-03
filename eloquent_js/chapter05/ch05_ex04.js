736eeadec89e5438d393d9808209cd158e4e635d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-10-03 11:11:48 +0200

    Add chapter 5, exercise 4

--- eloquent_js/chapter05/ch05_ex04.js
+++ eloquent_js/chapter05/ch05_ex04.js
@@ -1,10 +1,7 @@
 function dominantDirection(text) {
-    let scripts = countBy(text, char => {
-        let script = characterScript(char.codePointAt(0));
-        return script ? script.direction : "none";
-    }).filter(({name}) => name != "none");
-
-    if (counted.length == 0) return "ltr";
-
-    return scripts.reduce((a, b) => a.count > b.count ? a : b).name;
+	return countBy(text, char => {
+		let script = characterScript(char.codePointAt(0));
+		return script ? script.direction : "none";
+	}).filter(({name}) => name != "none").
+		reduce((a, b) => a.count > b.count ? a : b).name;
 }
