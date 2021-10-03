1c9039a0bc7fa585ed309cbc0ae73a29da93ca10
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-10-03 11:44:55 +0200

    Add chapter 6, exercise 1

--- eloquent_js/chapter06/ch06_ex01.js
+++ eloquent_js/chapter06/ch06_ex01.js
@@ -1,18 +1,18 @@
 class Vec {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    plus(v) {
-        return new Vec(this.x + v.x, this.y + v.y);
-    }
+	plus(v) {
+		return new Vec(this.x + v.x, this.y + v.y);
+	}
 
-    minus(v) {
-        return new Vec(this.x - v.x, this.y - v.y);
-    }
+	minus(v) {
+		return new Vec(this.x - v.x, this.y - v.y);
+	}
 
-    get length() {
-        return Math.sqrt(this.x**2 + this.y**2);
-    }
+	get length() {
+		return Math.sqrt(this.x**2 + this.y**2);
+	}
 }
