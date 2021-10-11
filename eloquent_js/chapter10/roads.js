1cbb01464c3fe479db53849b242dc6961969e0a4
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-10-11 17:57:59 +0200

    Add chapter 10, exercise 2

--- eloquent_js/chapter10/roads.js
+++ eloquent_js/chapter10/roads.js
@@ -1,17 +1,13 @@
-// Depend on ./graph module
-const {buildGraph} = require("./graph");
+const graph = require("./graph");
 
 const roads = [
-    "Alice's House-Bob's House",   "Alice's House-Cabin",
-    "Alice's House-Post Office",   "Bob's House-Town Hall",
-    "Daria's House-Ernie's House", "Daria's House-Town Hall",
-    "Ernie's House-Grete's House", "Grete's House-Farm",
-    "Grete's House-Shop",          "Marketplace-Farm",
-    "Marketplace-Post Office",     "Marketplace-Shop",
-    "Marketplace-Town Hall",       "Shop-Town Hall"
+	"Alice's House-Bob's House",   "Alice's House-Cabin",
+	"Alice's House-Post Office",   "Bob's House-Town Hall",
+	"Daria's House-Ernie's House", "Daria's House-Town Hall",
+	"Ernie's House-Grete's House", "Grete's House-Farm",
+	"Grete's House-Shop",          "Marketplace-Farm",
+	"Marketplace-Post Office",     "Marketplace-Shop",
+	"Marketplace-Town Hall",       "Shop-Town Hall"
 ];
 
-// Not really needed - could call buildGraph directly
-let roadArray = roads.map(r => r.split("-"));
-
-exports.roadGraph = buildGraph(roadArray);
+exports.roadGraph = graph.buildGraph(roads.map(r => r.split("-")));
