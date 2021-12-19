5e3048265879bc1cc47cbb21f11909f0cd427acb
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-12-19 15:21:57 -0500

    Add chapter 17, exercise 1, diamond

--- eloquent_js/chapter17/ch17_ex01.js
+++ eloquent_js/chapter17/ch17_ex01.js
@@ -1,6 +1,6 @@
 function trapezoid(cx, x, y, top, bottom, height) {
-	topOffset = top > bottom ? 0 : (bottom - top) / 2;
-	bottomOffset = bottom > top ? 0 : (top - bottom) / 2;
+	let topOffset = top > bottom ? 0 : (bottom - top) / 2;
+	let bottomOffset = bottom > top ? 0 : (top - bottom) / 2;
 
 	cx.beginPath();
 	cx.moveTo(x + topOffset, y);
@@ -11,6 +11,19 @@ function trapezoid(cx, x, y, top, bottom, height) {
 	cx.stroke();
 }
 
+function diamond(cx, x, y, side, col) {
+	let offset = side / Math.sqrt(2);
+
+	cx.save();
+	cx.translate(x + offset, y);
+	cx.rotate(Math.PI / 4);
+	cx.fillStyle = col;
+	cx.fillRect(0, 0, side, side);
+	cx.restore();
+}
+
 trapezoid(cx, 10, 10, 60, 80, 20);
 trapezoid(cx, 100, 10, 90, 40, 30);
 
+diamond(cx, 10, 60, 30, "red");
+diamond(cx, 60, 60, 45, "blue");
