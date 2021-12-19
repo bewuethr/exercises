1e5f62846d562b47ed706a704cc42d1afd376a82
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2021-12-19 17:44:42 -0500

    Add Chapter 17, exercise 1, star

--- eloquent_js/chapter17/ch17_ex01.js
+++ eloquent_js/chapter17/ch17_ex01.js
@@ -54,6 +54,21 @@ function spiral(cx, x, y, radius, n) {
 	cx.stroke();
 }
 
+function star(cx, x, y, r, nSpikes = 8, col = "khaki") {
+	let dPhi = 2 * Math.PI / nSpikes, phi = -0.5 * Math.PI;
+
+	cx.beginPath();
+	cx.moveTo(x, y - r);
+
+	for (let i = 0; i < nSpikes; ++i) {
+		cx.quadraticCurveTo(x, y, x + r * Math.cos(phi + dPhi), y + r * Math.sin(phi + dPhi));
+		phi += dPhi;
+	}
+
+	cx.fillStyle = col;
+	cx.fill();
+}
+
 trapezoid(cx, 10, 10, 60, 80, 20);
 trapezoid(cx, 110, 10, 90, 40, 30);
 
@@ -65,3 +80,6 @@ zigZag(cx, 430, 10, 25, 100, 50);
 
 spiral(cx, 500, 35, 25, 5);
 spiral(cx, 595, 60, 50, 4.75);
+
+star(cx, 695, 40, 30);
+star(cx, 795, 60, 50, 13, "orange");
