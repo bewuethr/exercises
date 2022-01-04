05e992cc086746b2f8270495e44cbf995db943cb
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2022-01-03 23:19:44 -0500

    Update usage instruction

--- eloquent_js/chapter20/ch20_ex01.js
+++ eloquent_js/chapter20/ch20_ex01.js
@@ -1,11 +1,9 @@
-#!/usr/bin/env node
-
 import process from "process";
 import {stat, readdir, readFile} from "fs/promises";
 import {sep} from "path";
 
 if (process.argv.length < 4) {
-	console.error("usage: ch20_ex01 REGEX FILE [FILE...]");
+	console.error("usage: node ch20_ex01.js REGEX FILE [FILE...]");
 	process.exit(1);
 }
 
